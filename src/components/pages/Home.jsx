bb6cb541d95997541d37d3b27f4c14ffb286fd93
first.cerberus <sans15365@gmail.com>
2025-09-03 09:44:29 +0300

    added mock data and map method for rectangular cards

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,35 +1,58 @@
-import React from "react";
-import RevenueCard from "../RevenueCard";
-import PeopleCard from "../PeopleCard";
 import RectangularCard from "../RectangularCard";
-import { LuHouse, LuArrowDown, LuArrowUp} from "react-icons/lu";
+import { LuArrowDown, LuArrowUp } from "react-icons/lu";
 import TestChart from "../TestChart";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const fakeApiResponse = [
+        {
+          title: "Доходи",
+          value: "$12,345",
+          percent: "+5%",
+          percentColor: "text-green-600",
+          percentBg: "bg-green-100",
+          description: "Загальний дохід за місяць",
+          icon: "up",
+        },
+        {
+          title: "Користувачі",
+          value: "1,234",
+          percent: "+10%",
+          percentColor: "text-red-600",
+          percentBg: "bg-red-100",
+          description: "Загальна кількість користувачів",
+          icon: "down",
+        },
+      ];
+
+      const mappedData = fakeApiResponse.map((item) => ({
+        ...item,
+        icon: item.icon === "up" ? <LuArrowUp /> : <LuArrowDown />,
+      }));
+
+      setCards(mappedData);
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) {
+    return <p>Завантаження</p>;
+  }
+
   return (
     <>
       <div className="flex flex-row gap-4">
-        <RectangularCard
-          title="Доходи"
-          value="$12,345"
-          icon={<LuArrowUp />}
-          percent="+5%"
-          percentColor="text-green-600"
-          percentBg="bg-green-100"
-          description="Загальний дохід за місяць"
-        />
-        <RectangularCard
-          title="Користувачі"
-          value="1,234"
-          icon={<LuArrowDown />}
-          percent="+10%"
-          percentColor="text-red-600"
-          percentBg="bg-red-100"
-          description="Загальна кількість користувачів"
-        />
+        {cards.map((card, index) => (
+          <RectangularCard key={index} {...card} />
+        ))}
       </div>
       <div className="mt-10 max-w-3xl">
-      <TestChart />
+        <TestChart />
       </div>
     </>
   );
